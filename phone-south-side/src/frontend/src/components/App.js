3f5436e15b6f886b692c0b967404c70fa094a482
MFAHAD3 <mohammedfahad@live.com>
2019-09-25 15:53:37 -0400

    RDM start, toggle button added + styling, App changed to class comp

--- phone-south-side/src/frontend/src/components/App.js
+++ phone-south-side/src/frontend/src/components/App.js
@@ -1,22 +1,60 @@
 import React, { lazy, Suspense } from "react";
 import Phone from "./Phone.js";
+import "./App.css";
 const SignalStrength = lazy(() => import("./SignalStrength"));
 
-const App = () => {
-  return (
-    <div>
-      <Phone />
-      <Suspense
-        fallback={
-          <div>
-            <h1 className="ui header blue loading">Loading your phone....</h1>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="headertoggle">
+          <div className="slide">
+            toggle here<i className="angle double right icon"></i>
           </div>
-        }
-      >
-        <SignalStrength />
-      </Suspense>
-    </div>
-  );
-};
+          <div className="ui toggle checkbox">
+            <input type="checkbox" name="public" />
+            <label></label>
+          </div>
+        </div>
+        <Phone />
+        <Suspense
+          fallback={
+            <div>
+              <h1 className="ui header blue loading">Loading your phone....</h1>
+            </div>
+          }
+        >
+          <SignalStrength />
+        </Suspense>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div className="headertoggle">
+//         <div className="slide">
+//           toggle here<i className="angle double right icon"></i>
+//         </div>
+//         <div className="ui toggle checkbox">
+//           <input type="checkbox" name="public" />
+//           <label></label>
+//         </div>
+//       </div>
+//       <Phone />
+//       <Suspense
+//         fallback={
+//           <div>
+//             <h1 className="ui header blue loading">Loading your phone....</h1>
+//           </div>
+//         }
+//       >
+//         <SignalStrength />
+//       </Suspense>
+//     </div>
+//   );
+// };
 
 export default App;
